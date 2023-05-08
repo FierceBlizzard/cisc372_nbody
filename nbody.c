156bdf30155143ae54c6f108131b134f2f10eb5e
Iclyn Taero <43586417+FierceBlizzard@users.noreply.github.com>
2023-05-08 10:59:35 -0400

    Update nbody.c

--- nbody.c
+++ nbody.c
@@ -8,9 +8,10 @@
 #include "compute.h"
 
 // represents the objects in the system.  Global variables
-__constant__ vector3 *hVel, *d_hVel;
-__constant__ vector3 *hPos, *d_hPos;
-__constant__ double *mass;
+// represents the objects in the system. Global variables
+__device__ vector3 *d_hVel;
+__device__ vector3 *d_hPos;
+__device__ double *d_mass;
 
 //initHostMemory: Create storage for numObjects entities in our system
 //Parameters: numObjects: number of objects to allocate
@@ -18,9 +19,9 @@ __constant__ double *mass;
 //Side Effects: Allocates memory in the hVel, hPos, and mass global variables
 void initHostMemory(int numObjects)
 {
-	hVel = (vector3 *)malloc(sizeof(vector3) * numObjects);
-	hPos = (vector3 *)malloc(sizeof(vector3) * numObjects);
-	mass = (double *)malloc(sizeof(double) * numObjects);
+    cudaMallocManaged(&d_hVel, sizeof(vector3) * numObjects);
+    cudaMallocManaged(&d_hPos, sizeof(vector3) * numObjects);
+    cudaMallocManaged(&d_mass, sizeof(double) * numObjects);
 }
 
 //freeHostMemory: Free storage allocated by a previous call to initHostMemory
@@ -29,9 +30,9 @@ void initHostMemory(int numObjects)
 //Side Effects: Frees the memory allocated to global variables hVel, hPos, and mass.
 void freeHostMemory()
 {
-	free(hVel);
-	free(hPos);
-	free(mass);
+    cudaFree(d_hVel);
+    cudaFree(d_hPos);
+    cudaFree(d_mass);
 }
 
 //planetFill: Fill the first NUMPLANETS+1 entries of the entity arrays with an estimation
